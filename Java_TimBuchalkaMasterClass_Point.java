040951fc1e6e7b47b6c265c8327795dd5bbeb07f
Pavel Kvasnička <94805399+pavel281185@users.noreply.github.com>
2024-03-13 18:09:30 +0100

    Create Java_TimBuchalkaMasterClass_Point.java

---
+++ Java_TimBuchalkaMasterClass_Point.java
@@ -0,0 +1,39 @@
+public class Point {
+    private int x;
+    private int y;
+    
+    public Point(){}
+    
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public void setY(int y){
+        this.y = y;
+    }
+    
+    public double distance(){
+        return distance(0,0);
+    }
+    
+    public double distance(Point a){
+        return distance(a.getX(), a.getY());
+    }
+    
+    public double distance(int x, int y){
+        return Math.sqrt(Math.pow(this.x-x,2)+Math.pow(this.y-y,2));
+    }
+}
